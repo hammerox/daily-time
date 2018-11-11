07de30251a6647eba631c0f04fc6926068b4dde2
Mauricio Custodio <mcustodio@stone.com.br>
2018-11-11 20:48:37 -0200

    Texts now persists.

---
+++ app/src/main/java/com/mcustodio/dailytime/Preferences.kt
@@ -0,0 +1,33 @@
+package com.mcustodio.dailytime
+
+import android.content.Context
+import android.content.SharedPreferences
+
+class Preferences(context: Context) {
+
+    val PREFS_FILENAME = "com.mcustodio.dailytime"
+    private val prefs: SharedPreferences = context.getSharedPreferences(PREFS_FILENAME, 0)
+
+
+    private val WHAT_YOU_DID = "WHAT_YOU_DID"
+    var whatYouDid: String?
+        get() = prefs.getString(WHAT_YOU_DID, "")
+        set(value) = prefs.edit().putString(WHAT_YOU_DID, value).apply()
+
+    private val WHAT_YOU_WILL_DO = "WHAT_YOU_WILL_DO"
+    var whatYouWillDo: String?
+        get() = prefs.getString(WHAT_YOU_WILL_DO, "")
+        set(value) = prefs.edit().putString(WHAT_YOU_WILL_DO, value).apply()
+
+    private val DIFFICULTIES = "DIFFICULTIES"
+    var difficulties: String?
+        get() = prefs.getString(DIFFICULTIES, "")
+        set(value) = prefs.edit().putString(DIFFICULTIES, value).apply()
+
+
+    fun clearAll() {
+        prefs.edit().clear().apply()
+    }
+
+
+}
--- app/src/main/java/com/mcustodio/dailytime/TimerActivity.kt
+++ app/src/main/java/com/mcustodio/dailytime/TimerActivity.kt
@@ -1,25 +1,34 @@
 package com.mcustodio.dailytime
 
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.SystemClock
+import android.support.v4.content.ContextCompat
 import android.view.View
 import android.widget.Toast
 import kotlinx.android.synthetic.main.timer_activity.*
 
 class TimerActivity : AppCompatActivity() {
 
+    private val preferences by lazy { Preferences(this) }
     private var timeWhenStopped = 0L
     private var isRunning = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.timer_activity)
+
+        edit_timer_whatyoudid.setText(preferences.whatYouDid)
+        edit_timer_whatyouwilldo.setText(preferences.whatYouWillDo)
+        edit_timer_difficulties.setText(preferences.difficulties)
+
         button_timer_startorpause.setOnClickListener {
             isRunning = !isRunning
             if (isRunning) {
                 chronometer_timer.base = SystemClock.elapsedRealtime() - timeWhenStopped
                 chronometer_timer.start()
+                chronometer_timer.setTextColor(ContextCompat.getColor(this, R.color.red_500))
 
                 button_timer_startorpause.text = "PAUSE"
                 button_timer_save.visibility = View.INVISIBLE
@@ -27,11 +36,12 @@ class TimerActivity : AppCompatActivity() {
             } else {
                 timeWhenStopped = SystemClock.elapsedRealtime() - chronometer_timer.base
                 chronometer_timer.stop()
+                chronometer_timer.setTextColor(ContextCompat.getColor(this, R.color.black))
 
                 button_timer_startorpause.text = "START"
                 button_timer_save.visibility = View.VISIBLE
             }
-         }
+        }
 
         button_timer_save.setOnClickListener {
             val userKey = intent.getStringExtra("userKey")
@@ -41,4 +51,11 @@ class TimerActivity : AppCompatActivity() {
         }
     }
 
+
+    override fun onPause() {
+        super.onPause()
+        preferences.whatYouDid = edit_timer_whatyoudid.text.toString()
+        preferences.whatYouWillDo = edit_timer_whatyouwilldo.text.toString()
+        preferences.difficulties = edit_timer_difficulties.text.toString()
+    }
 }
